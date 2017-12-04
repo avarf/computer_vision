c0d647b3aa11c5d9307e912e4f97e4c793cc2163
avarf <varfanm@live.com>
2017-12-04 19:04:04 +0100

    first working project of Emgu created that can show a photo

---
+++ emgucv_tutorial/Emgucv_Tutorial/Emgucv_Tutorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Emgu;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace Emgucv_Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "C:\\workspace\\git_repos\\datasets\\computer_vision\\modern.jpg";
+            Image<Bgr, byte> img = new Image<Bgr, byte>(filename);
+            CvInvoke.Imshow("image", img);
+            CvInvoke.WaitKey(0);
+
+
+        }
+    }
+}
