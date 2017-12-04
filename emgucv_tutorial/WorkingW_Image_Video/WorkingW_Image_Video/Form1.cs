04987cfd93778f21a2ef24477f3e403f0aa0c4b6
avarf <varfanm@live.com>
2017-12-05 00:10:59 +0100

    new project for opening and showing image using windows form created and works as expected

---
+++ emgucv_tutorial/WorkingW_Image_Video/WorkingW_Image_Video/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace WorkingW_Image_Video
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    Image<Bgr, byte> img = new Image<Bgr, byte>(ofd.FileName);
+                    pictureBox1.Image = img.Bitmap;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ pedestrian_detection/Pedestrian_detection/Pedestrian_detection/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System.Drawing;
+using System.Diagnostics;
+
+namespace Pedestrian_detection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch watch;
+            Rectangle[] regions;
+
+            else
+            {  //this is the CPU version
+                using (HOGDescriptor des = new HOGDescriptor())
+                {
+                    des.SetSVMDetector(HOGDescriptor.GetDefaultPeopleDetector());
+
+                    watch = Stopwatch.StartNew();
+                    regions = des.DetectMultiScale(image);
+                }
+            }
+            watch.Stop();
+
+            processingTime = watch.ElapsedMilliseconds;
+
+            foreach (Rectangle pedestrain in regions)
+            {
+                image.Draw(pedestrain, new Bgr(Color.Red), 1);
+            }
+            return image;
+        }
+    }
+}
+
+
