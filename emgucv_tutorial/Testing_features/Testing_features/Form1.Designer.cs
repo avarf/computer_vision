797625c92a5bce324a301340c7430cc42739ff34
avarf <varfanm@live.com>
2017-12-07 12:06:49 +0100

    working with Emgu Matrix like copying image into matrix or slicing and reshaping tests added in a new project. ToDo: calculating the HOG of an image - Training SVM

---
+++ emgucv_tutorial/Testing_features/Testing_features/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Testing_features
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ReadImg_button = new System.Windows.Forms.Button();
+            this.HOG_button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ReadImg_button
+            // 
+            this.ReadImg_button.Location = new System.Drawing.Point(13, 13);
+            this.ReadImg_button.Name = "ReadImg_button";
+            this.ReadImg_button.Size = new System.Drawing.Size(75, 23);
+            this.ReadImg_button.TabIndex = 0;
+            this.ReadImg_button.Text = "Read Img";
+            this.ReadImg_button.UseVisualStyleBackColor = true;
+            this.ReadImg_button.Click += new System.EventHandler(this.ReadImg_button_Click);
+            // 
+            // HOG_button
+            // 
+            this.HOG_button.Location = new System.Drawing.Point(13, 43);
+            this.HOG_button.Name = "HOG_button";
+            this.HOG_button.Size = new System.Drawing.Size(75, 23);
+            this.HOG_button.TabIndex = 1;
+            this.HOG_button.Text = "HOG";
+            this.HOG_button.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(104, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(898, 420);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.HOG_button);
+            this.Controls.Add(this.ReadImg_button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ReadImg_button;
+        private System.Windows.Forms.Button HOG_button;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ emgucv_tutorial/Testing_features/Testing_features/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.ML;
+using Emgu.CV.ML.Structure;
+
+namespace Testing_features
+{
+    public partial class Form1 : Form
+    {
+
+        Image<Bgr, byte> img;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ReadImg_button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    img = new Image<Bgr, byte>(ofd.FileName);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            Matrix<float> mat = new Matrix<float>(3, 5);
+            label1.Text = "ali";
+            // Matrix <Byte> mat = new Matrix<Byte>(img.Rows, img.Cols, img.NumberOfChannels);
+            // img.CopyTo(mat);
+
+            label1.Text = label1.Text + "\n";
+            //for (int r = 0; r < mat.Rows; r++)
+            //{
+            //    for (int c = 0; c < mat.Cols; c++)
+            //    {
+            //        mat.Data[r, c] = 1;
+            //    }
+            //}
+            mat.SetValue(9);
+            mat.Data[1, 1] = 55;
+            Matrix<float> remat;
+            remat = mat.Reshape(1, 1);
+
+            label1.Text = label1.Text + remat.Rows + " \n";
+            label1.Text = label1.Text + remat.Cols + " \n";
+
+            //for (int r = 0; r < remat.Rows; r++)
+            //{
+            //    for (int c = 0; c < remat.Cols; c++)
+            //    {
+            //        //label1.Text = label1.Text + mat.Data[r, c].ToString()+"II";
+            //        label1.Text = label1.Text + remat.Data[r, c].ToString() + "II";
+
+            //    }
+            //    label1.Text = label1.Text + "\n LINEE \n";
+            //}
+        }
+    }
+}
